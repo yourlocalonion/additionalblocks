bfc359b9d56738d157e8cb06712ada0a28ce0b8f
yourlocalonion <110649288+yourlocalonion@users.noreply.github.com>
2023-03-06 18:53:03 +0800

    Plastic Variants

---
+++ src/main/java/io/yourlocalonion/blockylicious/block/BasePlasticItem.java
@@ -0,0 +1,22 @@
+package io.yourlocalonion.blockylicious.block;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class BasePlasticItem extends Item {
+    public BasePlasticItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level,
+                                List<Component> componentList, TooltipFlag tooltipFlag) {
+        componentList.add(Component.translatable("tooltip.blockylicious.plastic"));
+    }
+}
--- src/main/java/io/yourlocalonion/blockylicious/item/MainItemsClass.java
+++ src/main/java/io/yourlocalonion/blockylicious/item/MainItemsClass.java
@@ -2,6 +2,7 @@ package io.yourlocalonion.blockylicious.item;
 
 import io.yourlocalonion.blockylicious.ABCreativeTab;
 import io.yourlocalonion.blockylicious.BlockyliciousMain;
+import io.yourlocalonion.blockylicious.block.BasePlasticItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -21,8 +22,67 @@ public class MainItemsClass {
                     .tab(ABCreativeTab.AB_CREATIVE_TAB)));
 
     public static final RegistryObject<Item> PLASTIC = ITEMS.register("plastic",
-            () -> new Item(new Item.Properties()
+            () -> new BasePlasticItem(new Item.Properties()
                     .tab(ABCreativeTab.AB_CREATIVE_TAB)));
+
+    public static final RegistryObject<Item> PLASTIC_RED = ITEMS.register("plastic_red",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_ORANGE = ITEMS.register("plastic_orange",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_YELLOW = ITEMS.register("plastic_yellow",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_LIME = ITEMS.register("plastic_lime",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_GREEN = ITEMS.register("plastic_green",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_CYAN = ITEMS.register("plastic_cyan",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_LIGHTBLUE = ITEMS.register("plastic_lightblue",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_BLUE = ITEMS.register("plastic_blue",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_PURPLE = ITEMS.register("plastic_purple",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_MAGENTA = ITEMS.register("plastic_magenta",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_PINK = ITEMS.register("plastic_pink",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+    public static final RegistryObject<Item> PLASTIC_BLACK = ITEMS.register("plastic_black",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_BROWN = ITEMS.register("plastic_brown",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_GRAY = ITEMS.register("plastic_gray",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
+
+    public static final RegistryObject<Item> PLASTIC_LIGHTGRAY = ITEMS.register("plastic_lightgray",
+            () -> new BasePlasticItem(new Item.Properties().tab(ABCreativeTab.AB_CREATIVE_TAB))
+    );
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
